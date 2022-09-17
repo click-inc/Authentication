f02fa8b6856c18c346f7418db6485efbb73c2598
Mahe077 <2019cs077@stu.ucsc.lk>
2022-09-17 18:13:46 +0530

    user authentication with jwt tokens

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const userRouter = require('./routers/user')
+
+require('./db/mongoose')
+
+const port = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log('server listening on port ' + port)
+})
